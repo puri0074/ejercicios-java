cd70735b5a7156d048c6b0e2cfb03de0b297a03a
puri0074 <1472315@senati.pe>
2023-03-25 09:57:01 -0500

    ejercicio02

---
+++ JavaApplication7.java
@@ -0,0 +1,15 @@
+
+package javaapplication7;
+
+public class JavaApplication7 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 1; i <= 999; i++)  {
+            if (i % 2 !=0) {
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
