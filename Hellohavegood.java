2862ffc8049ee36d658d0289f3f6d7437a7d19b8
puri0074 <1472315@senati.pe>
2023-03-25 09:20:04 -0500

    ejercicio01

---
+++ Hellohavegood.java
@@ -0,0 +1,14 @@
+public class Hellohavegood {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Morning, Morning, Morning");
+        System.out.println("        Hello, Have a good day");
+        System.out.println("                 oh! Thanks you");
+        System.out.println("                                    ");
+        System.out.println("                  You are great");
+        System.out.println("          this is my pleasureg");
+        System.out.println("No... You are welcome");
+        
+    }
+    
+}
